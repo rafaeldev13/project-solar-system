ddca66c76bc38ef4fae1eacbd3f92fc6adda5518
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-06-22 13:09:24 -0400

    Requisito 5 - Correcão

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class PlanetCard extends React.Component {
   render() {
-    const { planetName } = this.props;
-    const { planetImage } = this.props;
+    const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
         {planetName}
