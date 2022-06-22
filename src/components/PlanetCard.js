e9c1bc7f836e6a1202ea087387e3ef2100f97fbc
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-06-22 12:58:14 -0400

    Requisito 5

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName } = this.props;
+    const { planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        {planetName}
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>);
+  }
+}
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+
+export default PlanetCard;
